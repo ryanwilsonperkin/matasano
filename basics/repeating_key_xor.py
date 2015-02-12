7676231fbe511cfc418d63975c046c93cee3688d
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-12 18:47:37 -0500

    Fix offset in repeating_key_xor

--- basics/repeating_key_xor.py
+++ basics/repeating_key_xor.py
@@ -1,4 +1,4 @@
-from convert import ascii_to_bytes, bytes_to_hex
+from convert import ascii_to_bytes, bytes_to_hex, bytes_to_ascii
 from xor import xor_bytes
 
 def encrypt(clear, seq):
@@ -6,10 +6,15 @@ def encrypt(clear, seq):
     seq_repeated = seq * quotient + seq[:remainder]
     return xor_bytes(clear, seq_repeated)
 
+def rotate(s, offset):
+    return s[offset:] + s[:offset]
+
 if __name__ == "__main__":
     import sys
+    offset = 0
     key = ascii_to_bytes(sys.argv[1])
     for line in sys.stdin:
         clear = ascii_to_bytes(line)
-        cipher = encrypt(clear, key)
+        cipher = encrypt(clear, rotate(key, offset))
+        offset = (offset - len(clear) / len(key)) % len(key)
         print bytes_to_hex(cipher)
