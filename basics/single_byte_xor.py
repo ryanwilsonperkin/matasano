1189161dc20f02d7d3abdc8b7ccef000c053f2b8
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-11 21:23:39 -0500

    Factor out "candidate" code in single_byte script

--- basics/single_byte_xor.py
+++ basics/single_byte_xor.py
@@ -1,26 +1,28 @@
+from collections import namedtuple
 from sys import argv
 from convert import hex_to_bytes, bytes_to_hex
 from xor import xor_bytes
 
 COMMON_LETTERS = "ETAONRISHDL "
+Candidate = namedtuple('Candidate', ['key', 'clear_text', 'ranking'])
 
 def rank(s):
     return sum([s.count(c) + s.count(c.lower())  for c in COMMON_LETTERS])
 
+def candidates(byte_list):
+    candidates = [] 
+    for byte in range(0,256):
+        decoded = xor_bytes(byte_list, [byte] * len(byte_list))
+        clear_text = ''.join(chr(b) for b in decoded)
+        ranking = rank(clear_text)
+        candidates.append(Candidate(byte, clear_text, ranking))
+
+    return sorted(candidates, reverse=True, key=lambda x: x[2])
+
 if __name__ == "__main__":
     input_str = argv[1]
     input_bytes = hex_to_bytes(input_str)
-
-    candidates = [] 
-    for byte in range(0,256):
-        byte_arr = [byte for i in xrange(len(input_bytes))]
-        clear_text = ''.join(chr(b) for b in xor_bytes(byte_arr, input_bytes))
-        ranking = rank(clear_text)
-        candidates.append((byte, clear_text, ranking))
-
-    candidates.sort(reverse=True, key=lambda x: x[2])
-
-    top_candidate = candidates[0]
+    top_candidate = candidates(input_bytes)[0]
     print "HEX: {0}".format(bytes_to_hex([ord(c) for c in top_candidate[1]]))
-    print "ASCII: {0}".format(top_candidate[1])
-    print "XOR Byte: {0}".format(top_candidate[0])
+    print "ASCII: {0}".format(top_candidate.clear_text)
+    print "XOR Byte: {0}".format(top_candidate.key)
