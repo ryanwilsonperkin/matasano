327e8b1f1e40624d6dbbc9a6589329991b1d1d63
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-12 11:49:05 -0500

    Move sys import into main.

--- basics/detect_single_byte_xor.py
+++ basics/detect_single_byte_xor.py
@@ -1,9 +1,9 @@
-from sys import argv
 from convert import hex_to_bytes, bytes_to_hex, bytes_to_ascii
 from single_byte_xor import candidates
 
 if __name__ == "__main__":
-    input_file = argv[1]
+    import sys
+    input_file = sys.argv[1]
     candidate_list = []
     with open(input_file) as f:
         for line in f:
