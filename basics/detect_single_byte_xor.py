e096da054cda8ce02566aa7f710817af4518b32c
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-12 11:48:18 -0500

    Use bytes_to_ascii.

--- basics/detect_single_byte_xor.py
+++ basics/detect_single_byte_xor.py
@@ -1,5 +1,5 @@
 from sys import argv
-from convert import hex_to_bytes, bytes_to_hex
+from convert import hex_to_bytes, bytes_to_hex, bytes_to_ascii
 from single_byte_xor import candidates
 
 if __name__ == "__main__":
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     top_candidate = candidate_list[0]
     print "CIPHER: {0}".format(bytes_to_hex(top_candidate.cipher))
     print "CLEAR: {0}".format(bytes_to_hex(top_candidate.clear))
-    print "DECODED: {0}".format(''.join(chr(b) for b in top_candidate.clear))
+    print "DECODED: {0}".format(bytes_to_ascii(top_candidate.clear))
     print "XOR Byte: {0}".format(top_candidate.key)
