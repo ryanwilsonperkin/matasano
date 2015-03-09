c8ae32430869544ebb6b133f99e3053e1e27bd14
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-03-09 13:30:18 -0400

    Use a key generator rather than rotation and offset

--- basics/repeating_key_xor.py
+++ basics/repeating_key_xor.py
@@ -1,13 +1,15 @@
 from convert import ascii_to_bytelist, bytelist_to_hex, bytelist_to_str
 from xor import xor_bytelist
 
-def encrypt(clear, seq):
-    quotient, remainder = divmod(len(clear), len(seq))
-    seq_repeated = seq * quotient + seq[:remainder]
-    return xor_bytelist(clear, seq_repeated)
+def encrypt(clear, key):
+    key = key_generator(key)
+    key_repeated = [key.next() for c in clear]
+    return xor_bytelist(clear, key_repeated)
 
-def rotate(s, offset):
-    return s[offset:] + s[:offset]
+def key_generator(key):
+    while True:
+        for elem in key:
+            yield elem
 
 if __name__ == "__main__":
     import sys
@@ -19,6 +21,5 @@ if __name__ == "__main__":
         in_file = sys.stdin
     for line in in_file:
         clear = ascii_to_bytelist(line)
-        cipher = encrypt(clear, rotate(key, offset))
-        offset = (offset - len(clear) / len(key)) % len(key)
+        cipher = encrypt(clear, key)
         print bytelist_to_hex(cipher)
