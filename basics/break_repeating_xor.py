a0367ffc29eff8f214a5e981b97cea0cb2fa3409
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-03-09 23:54:06 -0400

    Factor avg_distance into hamming module

--- basics/break_repeating_xor.py
+++ basics/break_repeating_xor.py
@@ -1,15 +1,7 @@
 from convert import base64_to_bytelist, bytelist_to_str
-from hamming import distance
+from hamming import avg_distance
 from vignere import decrypt
 from single_byte_xor import candidates
-
-def average_distance(first_block, blocks):
-    n_blocks = 0
-    total = 0
-    for block in blocks:
-        n_blocks += 1
-        total += distance(first_block, block)
-    return float(total) / float(n_blocks)
 
 def split_list(bytelist, n):
     return zip(*[iter(bytelist)]*n)
@@ -19,7 +11,7 @@ def keysize_candidates(bytelist, keysize_min, keysize_max, n_blocks):
     for keysize in range(keysize_min, keysize_max):
         first_block = bytelist[:keysize]
         blocks = split_list(bytelist, keysize)[1:n_blocks+1]
-        average = average_distance(first_block, blocks)
+        average = avg_distance(first_block, *blocks)
         keysize_distance[keysize] = average / keysize
 
     return sorted(keysize_distance.items(), key=lambda (x,y): y)
--- basics/hamming.py
+++ basics/hamming.py
@@ -5,3 +5,11 @@ def distance(list1, list2):
     if len(list1) != len(list2):
         raise ValueError('lists must be of equal length')
     return sum(byte_distance(b1, b2) for b1, b2 in zip(list1, list2))
+
+def avg_distance(list1, list2, *lists):
+    distance_sum = distance(list1, list2)
+    n_lists = 1
+    for l in lists:
+        distance_sum += distance(list1, l)
+        n_lists += 1
+    return float(distance_sum) / float(n_lists)
